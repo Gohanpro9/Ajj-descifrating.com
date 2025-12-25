8825cc349b81ad6f01403628ebc23ac1c046ffbd
Gohanpro9 <166078096+Gohanpro9@users.noreply.github.com>
2025-12-24 19:10:47 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,20 @@
 const CACHE_NAME = "v0.5"
 
+self.addEventListener("install", e => {
+  self.skipWaiting()
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./App.js",
+        "./sw.js",
+        "./icon.png"
+      ])
+    )
+  )
+})
+
 self.addEventListener("activate", e => {
   e.waitUntil(
     caches.keys().then(keys =>
@@ -9,7 +24,3 @@ self.addEventListener("activate", e => {
     )
   )
 })
-
-
-self.skipWaiting()
-navigator.serviceWorker.addEventListener("controllerchange", () => location.reload())
